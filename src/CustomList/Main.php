223f51348d11a93f5380c00298dacd814847982c
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-26 19:53:22 +0000

    bug fixes, reupload.

--- src/CustomList/Main.php
+++ src/CustomList/Main.php
@@ -1,12 +1,7 @@
 <?php
 namespace CustomList;
-
-use pocketmine\utils\TextFormat;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 class Main extends PluginBase implements Listener {
     
@@ -15,15 +10,15 @@ public function onEnable(){
                 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info(TEXTFORMAT::BLUE . "[STAFF]" .TEXTFORMAT::RED. " >>" .TEXTFORMAT::AQUA.  " CustomList running version 1.0.0-ALPHA");
 	}
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
         $cmd = strtolower($command->getName());
         switch ($cmd){
             case "vmlist":
                 if (!($sender instanceof Player)){
-                    $getOnlinePlayers = $this->getServer()->getOnlinePlayers($sender->getName());
+                    $playerNames = $this->getServer()->getPlayers($sender->getName());
                     $getMaxPlayers = $this->getServer()->getMaxPlayers($sender->getName());
-                    $playerNames = $this->getServer()->getPlayerNames($playerNames->getName());
-                    $sender->sendMessage(TEXTFORMAT::GOLD . "§aThere is currently §b$getOnlinePlayers §aout of §b$getMaxPlayers §aonline.");
+                    $playersOnline = $this->getServer()->getPlayersOnline($sender->getName());
+                    $sender->sendMessage(TEXTFORMAT::GOLD . "§aThere is currently §b$playersOnline §aout of §b$getMaxPlayers §aonline.");
                     $sender->sendMessage(implode("§c, §7", $playerNames));
                     return true;
             }
