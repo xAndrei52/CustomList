780e48b6c42c407f89d50b99ff41fc8bcf7fb36d
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-26 19:21:21 +0000

    Hoping this works.

---
+++ src/CustomList/Main.php
@@ -0,0 +1,28 @@
+<?php
+namespace CustomList;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+class Main extends PluginBase implements Listener {
+    
+public function onEnable(){
+
+                $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TEXTFORMAT::BLUE . "[STAFF]" .TEXTFORMAT::RED. " >>" .TEXTFORMAT::AQUA.  " CustomList running version 1.0.0-ALPHA");
+	}
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        $cmd = strtolower($command->getName());
+        switch ($cmd){
+            case "list":
+                if (!($sender instanceof Player)){
+                    $getOnlinePlayers = $this->getServer()->getOnlinePlayers($sender->getName());
+                    $getMaxPlayers = $this->getServer()->getMaxPlayers($sender->getName());
+                    $playerNames = $this->getServer()->getPlayerNames($playerNames->getName());
+                    $sender->sendMessage(TEXTFORMAT::GOLD . "§aThere is currently §b$getOnlinePlayers §aout of §b$getMaxPlayers §aonline.");
+                    $sender->sendMessage(implode("§c, §7", $playerNames));
+                    return true;
+            }
+        }
+    }
+}
+
