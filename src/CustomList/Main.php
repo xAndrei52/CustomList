38f754d07eb2f9e52f4fe4ab24e748364a81f64e
iZeaoGamer <jameshayward14@icloud.com>
2018-04-01 15:13:43 +0100

    Fixed.

--- src/CustomList/Main.php
+++ src/CustomList/Main.php
@@ -1,41 +1,34 @@
 <?php
-
+ 
 namespace CustomList;
-
+ 
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\PlayerChatEvent;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Server;
 use pocketmine\Player;
-
+ 
 class Main extends PluginBase implements Listener {
     
 public function onEnable(){
                 $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info(TextFormat::BLUE . "[STAFF]" .TextFormat::RED. " >>" .TEXTFORMAT::AQUA.  " CustomList running version 1.0.0-ALPHA");
-	}
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+                $this->getLogger()->info(TextFormat::BLUE . "[STAFF]" .TextFormat::RED. " >>" .TEXTFORMAT::AQUA.  " CustomList running version 1.0.0-ALPHA");
+        }
+        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
         $cmd = strtolower($command->getName());
         switch ($cmd){
             case "vmlist":
                 if (!($sender instanceof Player)){
                     foreach($this->getServer()->getOnlinePlayers() as $player) {
-		            $maxPlayers = $this->getServer()->getMaxPlayers();
-			    $onlineList = $this->getServer()->getLoggedInPlayers();
+                            $maxPlayers = $this->getServer()->getMaxPlayers();
+                            $onlineList = $this->getServer()->getLoggedInPlayers();
                             $sender->sendMessage(TextFormat::GOLD . "§bThere are currently §3$onlineList §bonline! §aHere are the online players.\n§dimplode("," $player");
                             return true;
                  }
-             } else {
-		}
-	        if (!($sender instanceof ConsoleCommandSender)){
-			    $this->getServer()->getLogger()->info("Please run this command in game");
-                            return true;
-                           }
-                   }
-	        return true;
+               }
+                return true;
             }
-}
+    }
